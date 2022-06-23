03008a6f552c88515499303e7b822f69b777b2ab
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-06-23 16:35:49 -0300

    Código com Condicional Ternário

---
+++ src/ternario/CondicionalTernario.java
@@ -0,0 +1,21 @@
+package ternario;
+
+public class CondicionalTernario {
+
+	public static void main(String[] args) {
+		int a;
+		String b;
+		/*
+		a = 12;
+		b = (a == 5) ? "a é igual a 5": "a é diferente de 5";
+		System.out.println("Resultado: " + b);
+		
+		b = (a == 12) ? "a é igual a 12": "a é diferente de 12";
+		System.out.println("Resultado: " + b);
+		*/
+		a = 15;
+		b = (a > 14) ? "Atrasado": (a < 14) ? "Adiantado" : "No horário";
+		System.out.println("Resultado: " + b);
+	}
+
+}
