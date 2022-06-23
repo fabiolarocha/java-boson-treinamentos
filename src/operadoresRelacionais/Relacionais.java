d35751d9b512a0b8a4525044ac45060c7cd87004
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-06-23 16:16:10 -0300

    Código com Operadores Relacionais

---
+++ src/operadoresRelacionais/Relacionais.java
@@ -0,0 +1,17 @@
+package operadoresRelacionais;
+
+public class Relacionais {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int k = 20;
+		System.out.printf("x é igual a 10? %b\n", x == 10);
+		System.out.printf("x é diferente de 10? %b\n", x != 10);
+		System.out.printf("x é maior que k? %b\n", x > k);
+		System.out.printf("x é maior ou igual a 10? %b\n", x >= 10);
+		System.out.printf("x é menor que 10? %b\n", x < 10);
+		System.out.printf("x é menor ou igual a k? %b", x <= k);
+
+	}
+
+}
