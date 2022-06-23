51dd29b3a6c75aa3bac3b010a4724bfe1815b407
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-06-23 11:36:08 -0300

    Declaração e uso de constantes

---
+++ src/teste/ClasseScannerConstante.java
@@ -0,0 +1,21 @@
+package teste;
+
+import java.util.Scanner;
+public class ClasseScannerConstante {
+	private static final double LARGURA = 10.0;
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Entre com o comprimento: ");
+		double compr = entrada.nextDouble();
+		
+		double area = calculaArea(LARGURA, compr);
+		System.out.println("A área é: " + area);	
+	}
+		
+	private static double calculaArea(double largura, double comprimento) {
+		return largura * comprimento;
+	}
+
+}
